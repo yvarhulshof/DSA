699219410b23be712a366bf4e5538978133ba5b6
yvarhulshof <yvarhulshof@hotmail.com>
2018-03-03 20:20:07 +0100

    stuff

---
+++ Lab1/P2/Queue2.java
@@ -0,0 +1,39 @@
+public class Queue2<E>{
+
+  private StackImplementation<E> stack;
+  private StackImplementation<E> tempStack;
+
+  public Queue2(int givenSize){
+    stack = new StackImplementation(givenSize);
+    tempStack = new StackImplementation(givenSize);
+  }
+
+  public void enqueue(E e){
+    stack.push(e);
+  }
+
+  public E dequeue(){
+    if(stack.size() == 0) return null;
+
+    for(int i = 0; i < stack.size(); i++)
+    {
+      tempStack.push(stack.pop());
+    }
+
+    for(int i = 0; i < tempStack.size(); i++)
+    {
+      stack.push(tempStack.pop());
+    }
+    return tempStack.pop();
+  }
+
+  public void printElements(){
+    E[] elements = (E[]) stack.getElements();
+
+    for(int i = 0; i < stack.size(); i++)
+    {
+      System.out.println(elements[i]);
+    }
+  }
+
+}
